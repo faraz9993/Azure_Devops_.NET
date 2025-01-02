7c75370b1265636b10d9ec2ce10126248332c129
Farajnazish Ansari <faraj.ansari@einfochips.com>
2025-01-02 11:29:04 +0530

    Updating Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,38 +1,49 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace HelloWorldApp
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); 
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+                app.UseHsts(); 
             }
 
-            app.UseStaticFiles();
+            app.UseStaticFiles(); 
 
-            app.UseRouting();
+            app.UseRouting(); 
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    context.Response.ContentType = "text/html";
-                    await context.Response.WriteAsync("<html><body><img src='/images/DevOps.jpg' alt='DevOps Image' /></body></html>");
+                    context.Response.ContentType = "image/jpeg";
+                    var filePath = env.WebRootPath + "/DevOps.jpg"; 
+                    await context.Response.SendFileAsync(filePath); 
                 });
             });
         }
