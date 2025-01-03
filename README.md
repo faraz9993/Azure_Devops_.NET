b66a9a5600b5d659f7e549180b372f0cba6ff8fd
Farajnazish Ansari <faraj.ansari@einfochips.com>
2025-01-03 10:57:06 +0530

    Updating README file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Azure_Devops_.NET
+# Deployment of .NET application on Azure Function app and Web app.
+
+### First of alll, we need to have several files to make the .NET code work successfully.
+
+----
+1. Program.cs
+2. Startup.cs
+3. HelloWorld.csproj
+4. Azure_Devops_.NET.sln
+----
+
+### Make sure .NET  application is running successfully in local. 
--- obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad189eb92b0c899773b168f3b8d530663c92e41e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc5ecc85af3e7ed4170919f230b862bb7a1451b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
